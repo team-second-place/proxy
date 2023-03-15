de72fac31d071eff0f31297bdd14e402c31de626
J / Jacob Babich <jacobbabichpublic+git@gmail.com>
2023-03-15 16:49:18 -0400

    ⚡ feat: display crate name and version on the homepage to help with diagnosing future potential issues

--- src/routes/mod.rs
+++ src/routes/mod.rs
@@ -1,15 +1,24 @@
-use axum::Router;
+use axum::{routing::get, Router};
 
 use crate::AppState;
 
 mod microcontroller;
 mod user;
 
+pub static ROOT: &str = "/";
 pub static MICROCONTROLLER: &str = "/microcontroller";
 pub static USER: &str = "/user";
 
+const CRATE: &str = env!("CARGO_PKG_NAME");
+const VERSION: &str = env!("CARGO_PKG_VERSION");
+
+async fn handle_get() -> String {
+    format!("{CRATE} {VERSION}")
+}
+
 pub fn create_router() -> Router<AppState> {
     Router::new()
+        .route(ROOT, get(handle_get))
         .nest(MICROCONTROLLER, microcontroller::create_router())
         .nest(USER, user::create_router())
 }
